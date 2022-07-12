9507cecc3b1dd34f374d509af9e51da3a1f99991
sunbeom <wsbs9805@naver.com>
2022-07-12 17:55:50 +0900

    :memo: MsgScreen 구성 작업
    메시지 스크린 페이지 구성 작업
    디자인은 완전 배제 된...
    #3

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import {Text} from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 
 function FeedScreen() {
-  return <Text>test</Text>;
+  const [GoToMsg, setGoToMsg] = useState('holly shit');
+  // useEffect(()=>{
+  //   setGoToMsg();
+  // }, [])
+  const ChangeMSG = () => {
+    setGoToMsg('흐앙...');
+  }
+
+  // const goToMsgScreen () => {
+    
+  // }
+
+  return (
+    <View>
+      <View>
+        <Text onPress={ChangeMSG}>{GoToMsg}</Text>
+        {/* <Button onPress={()=> goToMsgScreen()} title="대충 메시지 화면 좀 봅시다 그려" /> */}
+      </View>
+    </View>
+  ) 
 }
 
 export default FeedScreen;
