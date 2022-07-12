e2ee60073cbb88ea69fd811cb68670ad3c1d2511
da-in <talown@naver.com>
2022-07-12 16:47:47 +0900

    :sparkles: Title Component
    Create Title Component #4

---
+++ components/Title.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface Props {
+  title: string;
+  alignCenter?: boolean;
+  subTitle?: string;
+}
+
+function Title({title, alignCenter = false, subTitle}: Props) {
+  console.log(alignCenter);
+  return (
+    <View style={alignCenter && styles.center}>
+      {subTitle && <Text style={styles.subTitle}>{subTitle}</Text>}
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+  },
+  subTitle: {
+    fontSize: 16,
+    color: 'black',
+    opacity: 0.4,
+  },
+  center: {
+    alignItems: 'center',
+  },
+});
+
+export default Title;
