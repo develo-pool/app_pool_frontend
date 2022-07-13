c80a14c6d973863720a8c7a367f6c08a2f18f413
sunbeom <wsbs9805@naver.com>
2022-07-13 16:35:19 +0900

    :memo: 컴포넌트 하위폴더 생성 및 파일명 수정
    #3

--- screens/MessageScreen.tsx
+++ screens/MessageScreen.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
-import BorderLine from '../components/BorderLine';
-import CommentContainer from '../components/CommentContainer';
-import CommentBar from '../components/CommentBar';
-import DetailMessageContainer from '../components/DetailMessageContainer';
-import InputCommentContainer from '../components/InputCommentContainer';
+import BorderLine from '../components/message/BorderLine';
+import Comment from '../components/message/Comment';
+import CommentBar from '../components/message/CommentBar';
+import DetailMessageContainer from '../components/message/DetailMessageContainer';
+import InputCommentContainer from '../components/message/InputCommentContainer';
 
 function MessageScreen() {
   // const [msgImgHeight, setmsgImgHeight] = useState(0);
@@ -18,7 +18,7 @@ function MessageScreen() {
 
         <CommentBar commentCount={927} />
 
-        <CommentContainer
+        <Comment
           user={undefined}
           commentText="프론트짱 다인님의 기운을 받아 햅삐ㅣㅣㅣㅣㅣㅣ"
           commentDate={Date.now()}
