a4594a662be1fd470aa4db054fe71f4cfe5295fb
sunbeom <wsbs9805@naver.com>
2022-07-14 11:13:52 +0900

    :art: 껍데기를 만들었어요
    각 요소들을 생성하고, 스타일을 입히기 위한 껍데기를 제작했습니다..!
    #1

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
-import { Text, View, ScrollView, StyleSheet, TextInput, Button } from 'react-native';
+import {
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  Button,
+  Image,
+} from 'react-native';
 import Title from '../components/Title';
+
+interface User {
+  name: string;
+  profileImg: string;
+  intro: string;
+  follower: number;
+}
+
+const hoon: User = {
+  name: 'hoon',
+  profileImg: 'https://reactnative.dev/img/tiny_logo.png',
+  intro: '훈훈훈릠릠릠오오오늘늘늘수수수민민민화화화이이이팅팅팅',
+  follower: 300,
+};
 
 function SearchScreen() {
   return (
@@ -8,16 +30,33 @@ function SearchScreen() {
       <ScrollView>
         <Title title="탐색하기" alignCenter={false} />
         <View style={styles.searchBar}>
-        <TextInput
-        //   value={text}
-        //   onChangeText={onChangeText}
-        //   onSubmitEditing={addToDo}
-        returnKeyType="go"
-        placeholder={'브랜드명을 검색해주세요.'}
-        style={styles.input}
-      />
-      <Button onPress={undefined} title="🥕" />
-      </View>
+          <TextInput
+            //   value={text}
+            //   onChangeText={onChangeText}
+            //   onSubmitEditing={addToDo}
+            returnKeyType="go"
+            placeholder={'브랜드명을 검색해주세요.'}
+            style={styles.input}
+          />
+          <Button onPress={undefined} title="🥕" />
+        </View>
+        <View style={styles.subTitleContainer}>
+          <Text style={styles.subTitle}>추천브랜드</Text>
+        </View>
+        <ScrollView style={styles.recommandBrandUserList}>
+          <View style={styles.recommandBrandUser}>
+            <Image
+              style={styles.brandUserProfileImg}
+              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+            />
+            <Text style={styles.brandUsername}>{hoon.name}</Text>
+            <View style={styles.brandUserFollowerContainer}>
+              <Text>팔로우</Text>
+              <Text>{hoon.follower}</Text>
+            </View>
+            <Button onPress={undefined} title="팔로우" />
+          </View>
+        </ScrollView>
       </ScrollView>
     </View>
   );
@@ -46,6 +85,13 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: 'FFFFFF',
   },
+  subTitle: {},
+  subTitleContainer: {},
+  brandUserProfileImg: {},
+  recommandBrandUserList: {},
+  recommandBrandUser: {},
+  brandUsername: {},
+  brandUserFollowerContainer: {},
 });
 
 export default SearchScreen;
