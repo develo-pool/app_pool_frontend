436b58a149a527cc926016729f1bf6a8a79c2ccb
da-in <talown@naver.com>
2022-07-14 18:00:43 +0900

    :sparkles: Login Screen Feature
    Active login button when TextInput is filled, Add login form state #4

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,37 +1,47 @@
-import {useNavigation} from '@react-navigation/native';
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {RootStackNavigationProp} from './RootStack';
 import MainContainer from '../components/MainContainer';
 import ScreenBottomButton from '../components/ScreenBottomButton';
 import Title from '../components/Title';
-import {RootStackNavigationProp} from './RootStack';
 import {InputTitle} from '../components/LoginComponents';
 
 function LoginScreen() {
   const navigation = useNavigation<RootStackNavigationProp>();
-  const [id, setId] = useState<string>('');
+  const [form, setForm] = useState({
+    phoneNum: '',
+    password: '',
+  });
+
+  const createChangeTextHandler = (name: string) => (value: string) => {
+    setForm({...form, [name]: value});
+  };
+
+  const passwordRef = useRef<TextInput | null>(null);
 
   return (
     <>
       <MainContainer>
         <View style={styles.block}>
           <Title title="로그인" />
-          <View>
-            <InputTitle title="로그인" />
-            <TextInput
-              style={styles.borderedInput}
-              placeholder="닉네임 입력"
-              value={id}
-              onChangeText={setId}
-            />
-          </View>
-          <View>
-            <InputTitle title="비밀번호" />
-            <TextInput
-              style={styles.borderedInput}
-              placeholder="비밀번호 입력"
-            />
-          </View>
+          <InputTitle title="휴대전화" />
+          <TextInput
+            style={styles.input}
+            value={form.phoneNum}
+            placeholder="'-'빼고 입력"
+            onChangeText={createChangeTextHandler('phoneNum')}
+            onSubmitEditing={() => passwordRef.current?.focus()}
+            returnKeyType="next"
+          />
+          <InputTitle title="비밀번호" />
+          <TextInput
+            style={styles.input}
+            value={form.password}
+            placeholder="비밀번호 입력"
+            onChangeText={createChangeTextHandler('password')}
+            ref={passwordRef}
+          />
           <View style={styles.wrapper}>
             <Pressable onPress={() => navigation.navigate('SignUp')}>
               <Text>회원가입</Text>
@@ -46,7 +56,7 @@ function LoginScreen() {
       <ScreenBottomButton
         name="로그인"
         onPress={() => navigation.navigate('SignUp')}
-        enabled={true}
+        enabled={Boolean(form.phoneNum) && Boolean(form.password)}
       />
     </>
   );
@@ -59,8 +69,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
-  borderedInput: {
+  input: {
+    borderWidth: 1,
+    borderColor: '#BABABA',
     backgroundColor: 'white',
+    marginBottom: 30,
+    paddingHorizontal: 12,
   },
 });
 
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainContainer from '../components/MainContainer';
 import ScreenBottomButton from '../components/ScreenBottomButton';
 import Title from '../components/Title';
+import {InputTitle} from '../components/LoginComponents';
+import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
 
 function SignUpScreen() {
+  const [form, setForm] = useState({
+    phoneNum: '',
+    password: '',
+    birthday: '',
+  });
+
+  const createChangeTextHandler = (name: string) => (value: string) => {
+    setForm({...form, [name]: value});
+  };
   return (
     <>
       <MainContainer>
         <Title title="기본정보" subTitle="회원가입" />
+        <InputTitle title="휴대폰 번호" />
+        <View style={styles.row}>
+          <TextInput
+            style={styles.input}
+            placeholder="01012345678"
+            value={form.phoneNum}
+            onChangeText={createChangeTextHandler('phoneNum')}
+          />
+          <Pressable>
+            <Text>중복확인</Text>
+          </Pressable>
+        </View>
+        <InputTitle title="성별" />
+        <View style={[styles.row, styles.margin]}>
+          <Pressable>
+            <Text>여자</Text>
+          </Pressable>
+          <Pressable>
+            <Text>남자</Text>
+          </Pressable>
+        </View>
+        <InputTitle title="생년월일" />
+        <TextInput
+          style={styles.input}
+          placeholder="예. 990101"
+          value={form.birthday}
+          onChangeText={createChangeTextHandler('phoneNum')}
+        />
       </MainContainer>
       <ScreenBottomButton name="다음" onPress={() => {}} enabled={true} />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+  margin: {
+    marginBottom: 24,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#BABABA',
+    backgroundColor: 'white',
+    marginBottom: 30,
+    paddingHorizontal: 12,
+  },
+});
 
 export default SignUpScreen;
