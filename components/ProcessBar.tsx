42bba71b29f1c6ce651d3adaafc5c1c270529803
da-in <talown@naver.com>
2022-07-15 13:29:04 +0900

    :sparkles: Create Process Bar
    Create Process Bar!

---
+++ components/ProcessBar.tsx
@@ -0,0 +1,47 @@
+// import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
+import {Animated, StyleSheet, View} from 'react-native';
+
+const WIDTH = 167;
+const HEIGHT = 4;
+
+function ProcessBar({total, current}: {total: number; current: number}) {
+  const currentSize = WIDTH / total;
+  const animation = useRef<Animated.Value>(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: (current - 1) * currentSize,
+      useNativeDriver: true,
+    }).start();
+  }, [animation, current, currentSize]);
+
+  return (
+    <View style={styles.total}>
+      <Animated.View
+        style={[
+          styles.current,
+          {width: currentSize, transform: [{translateX: animation}]},
+        ]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  total: {
+    backgroundColor: 'rgba(15,22,30,0.35)',
+    width: WIDTH,
+    height: HEIGHT,
+    zIndex: 0,
+  },
+  current: {
+    position: 'absolute',
+    backgroundColor: 'black',
+    width: WIDTH / 2,
+    height: HEIGHT,
+    zIndex: 5,
+  },
+});
+
+export default ProcessBar;
