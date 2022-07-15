6d6da346a8b97170cf228012d8b826cca6ddfd1e
sunbeom <wsbs9805@naver.com>
2022-07-15 11:53:17 +0900

    :art: Feed 컴포넌트 생성 및 FeedScreen 레이아웃 구현
    작성 완료 ! PR 이후에 개발 문서에 컴포넌트 문서 작성할게요 :)
    #2

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,26 +2,44 @@ import React, {useState} from 'react';
 import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 
 interface Props {
-  user: User | undefined;
-  commentText: string;
-  commentDate: number;
+  user: User;
+  message: Message;
+  isProfile?: boolean;
 }
-// 얘네는 코멘트를 작성한 유저들 !
+
+// !!아래의 인터페이스와 객체들은 향후에 hook으로 분리할 듯 합니당 나중에 프롭스만 전달해주는걸로!!
+// 얘네는 유저들 !
 interface User {
   name: string;
   profileImg: string;
 }
-// 유저 예시를 위한 dain 객체
-const beom: User = {
-  name: '다인님 제발',
-  profileImg: 'https://reactnative.dev/img/tiny_logo.png',
-};
+interface Message {
+  user: User | undefined;
+  isComment?: boolean;
+  isImg?: boolean;
+  isLink?: boolean;
+}
+
+// 유저 예시를 위한 doha 객체
+// const doha: User = {
+//   name: '이이이도도도하하하',
+//   profileImg: 'https://reactnative.dev/img/tiny_logo.png',
+// };
+// // 메시지 예시를 위한 test 객체
+// const test: Message = {
+//   user: doha,
+//   isComment: false,
+//   isImg: true,
+//   isLink: false,
+// };
 
 // 사용 시에는 user 프롭스를 아래에 넣어주세용
-function Feed() {
+function Feed({user, message, isProfile = false}: Props) {
+  // 높이를 정해주기 위한 height useState와 Dimensions 사용
   const [height, setHeight] = useState(0);
   const {width} = Dimensions.get('window');
-  Image.getSize(beom.profileImg, (w, h) => {
+  // 아래는 이미지의 가로 세로를 가져와 setHeight로 높이 설정해주는..!
+  Image.getSize(user.profileImg, (w, h) => {
     setHeight(h * (width / w));
   });
   return (
@@ -30,32 +48,41 @@ function Feed() {
         <View style={styles.feedOwner}>
           <Image
             style={styles.feedOwnerProfileImg}
-            source={{uri: `${beom.profileImg}`, height}}
-            resizeMode='cover'
+            source={{uri: `${user.profileImg}`}}
+            resizeMode="cover"
           />
-          <Text style={styles.feedOwnerUsername}>이이이도도도하하하</Text>
+          <Text style={styles.feedOwnerUsername}>{user.name}</Text>
         </View>
         <Text style={styles.feedDate}>방금 전</Text>
       </View>
       <View style={styles.feed}>
         <Text style={styles.feedText}>이거슨 피드본문</Text>
-        <View>
-        <Image
-          style={
-            //   styles.feedImg
-            {width:'100%', height:"100%"}
-            }
-          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-          resizeMode='contain'
-        />
+        {message.isImg ? (
+          <Image
+            style={styles.feedImg}
+            source={{uri: 'https://reactnative.dev/img/tiny_logo.png', height}}
+            resizeMode="contain"
+          />
+        ) : (
+          ''
+        )}
+        {message.isLink ? (
+          <Text style={styles.feedLink}>이거슨 피드링크</Text>
+        ) : (
+          ''
+        )}
+      </View>
+      {isProfile ? (
+        ''
+      ) : (
+        <View style={styles.isComment}>
+          {message.isComment ? (
+            <Text>🖤답장완료</Text>
+          ) : (
+            <Text>💚답장하기</Text>
+          )}
         </View>
-        <Text style={styles.feedLink}>이거슨 피드링크</Text>
-      </View>
-      <View style={styles.isComment}>
-        <Text>💚</Text>
-        <Text>답장완료</Text>
-        <Text>답장하기</Text>
-      </View>
+      )}
     </View>
   );
 }
@@ -97,11 +124,15 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   feedText: {},
-//   feedImg: {
-//     width: '100%',
-//   },
+  feedImg: {
+    width: '100%',
+  },
   feedLink: {},
-  isComment: {flexDirection: 'row'},
+  isComment: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: 10,
+  },
 });
 
 export default Feed;
--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,75 +1,64 @@
-import {Text, View, StyleSheet, Image, ScrollView} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import React from 'react';
 import Title from '../components/Title';
 import Feed from '../components/Feed';
 
-interface Props {
-  user: User | undefined;
-  commentText: string;
-  commentDate: number;
-}
-// 얘네는 코멘트를 작성한 유저들 !
 interface User {
   name: string;
   profileImg: string;
 }
-// 유저 예시를 위한 dain 객체
-const beom: User = {
-  name: '다인님 제발',
+interface Message {
+  user: User | undefined;
+  isComment?: boolean;
+  isImg?: boolean;
+  isLink?: boolean;
+}
+
+// 유저 예시를 위한 doha 객체
+const doha: User = {
+  name: '이이이도도도하하하',
   profileImg: 'https://reactnative.dev/img/tiny_logo.png',
+};
+// 메시지 예시를 위한 test 객체
+const test: Message = {
+  user: doha,
+  isComment: false,
+  isImg: true,
+  isLink: false,
 };
 
 function FeedScreen() {
   return (
     <View>
       <ScrollView>
-        <Title title="피드" />
-        <View>
-          <Text>2</Text>
-          <Text>팔로잉</Text>
+        <View style={styles.feedScreenHeader}>
+          <Title title="피드" />
+          <View style={styles.followingCount}>
+            <Text style={styles.followingCountText}>2</Text>
+            <Text style={styles.followingCountText}>팔로잉</Text>
+          </View>
         </View>
-        <Feed/>
-
-        {/* <View style={styles.feedContainer}>
-          <View style={styles.feedOwner}>
-            <View>
-              <Image
-                style={styles.feedOwnerProfileImg}
-                source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-              />
-              <Text style={styles.feedOwnerUsername}>이이이도도도하하하</Text>
-            </View>
-            <Text style={styles.feedDate}>방금 전</Text>
-          </View>
-          <View style={styles.feed}>
-            <Text style={styles.feedText}>이거슨 피드본문</Text>
-            <Image
-              style={styles.feedImg}
-              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-            />
-            <Text style={styles.feedLink}>이거슨 피드링크</Text>
-          </View>
-          <View>
-            <Text>💚</Text>
-            <Text>답장완료</Text>
-            <Text>답장하기</Text>
-          </View>
-        </View> */}
+        <Feed user={doha} message={test} isProfile={false} />
+        <Feed user={doha} message={test} isProfile={false} />
+        <Feed user={doha} message={test} isProfile={false} />
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  feedContainer: {},
-  feedOwner: {},
-  feedOwnerProfileImg: {},
-  feedOwnerUsername: {},
-  feedDate: {},
-  feed: {},
-  feedText: {},
-  feedImg: {},
-  feedLink: {},
+  feedScreenHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  followingCount: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  followingCountText: {
+    fontSize: 18,
+  },
 });
 
 export default FeedScreen;
