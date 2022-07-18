2b5bc16cf4d01c2ca8d2a9844b1d6ff346be17e7
Orca1110 <hoony4509@naver.com>
2022-07-18 14:00:24 +0900

    :art: Mid Check Setting Stck
    Middle Commit #9

--- screens/SettingStack.tsx
+++ screens/SettingStack.tsx
@@ -1,68 +1,104 @@
-import React from 'react';
-import {Text, View, StyleSheet, Pressable, Image} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Pressable, Image, Switch} from 'react-native';
 import Title from '../components/Title';
 import MainContainer from '../components/MainContainer';
 
 function SettingStack() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
-    <MainContainer>
-      <Title title="설정" alignCenter={false} />
-      <View style={styles.UserInfo}>
-        <View style={styles.ProfileImgContainer}>
-          <View>
-            <Image //Image로 바꿔줘야함!!
-              style={styles.ImgSource}
-              source={require('../assets/Pool.png')}
+    <View style={{flex: 1}}>
+      <View style={styles.UpperContainer}>
+        <Title title="설정" alignCenter={false} />
+        <View style={styles.UserInfoContainer}>
+          <View style={styles.ProfileImgContainer}>
+            <View>
+              <Image //Image로 바꿔줘야함!!
+                style={styles.ImgSource}
+                source={require('../assets/Pool.png')}
+              />
+            </View>
+            <View>
+              <Pressable>
+                <Image
+                  style={styles.EditProfile}
+                  source={require('../assets/Edit.png')}
+                />
+              </Pressable>
+            </View>
+          </View>
+          <View style={styles.UserNameContainer}>
+            <Text style={styles.UserName}>호빵맨</Text>
+          </View>
+          <View style={styles.FollowingContainer}>
+            <View>
+              <Text style={styles.FollowingCount}>1</Text>
+            </View>
+            <View>
+              <Text style={styles.Following}>팔로잉</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.JoinBrandContainer}>
+          <View style={styles.JoinImgContainer}>
+            <View //Image로 바꿔줘야함!!
+              style={styles.JoinBrandImg}
+              // source={require('../assets/Pool.png')}
             />
           </View>
-          <View>
-            <Pressable>
-              <Image
-                style={styles.EditProfile}
-                source={require('../assets/Edit.png')}
-              />
-            </Pressable>
-          </View>
-        </View>
-        <View style={styles.UserNameContainer}>
-          <Text style={styles.UserName}>호빵맨</Text>
-        </View>
-        <View style={styles.FollowingContainer}>
-          <View>
-            <Text style={styles.FollowingCount}>1</Text>
-          </View>
-          <View>
-            <Text style={styles.Following}>팔로잉</Text>
+          <View style={styles.JoinInfoContainer}>
+            <View style={styles.JoinTitleContainer}>
+              <Text style={styles.JoinBrandTitle}>브랜드 등록 신청하기</Text>
+            </View>
+            <View style={styles.JoinIntroContainer}>
+              <Text style={styles.JoinBrandIntro}>
+                누구나 브랜드로 등록하면 메시지를 보낼 수 있어요!
+              </Text>
+            </View>
           </View>
         </View>
       </View>
-      <View style={styles.JoinBrandContainer}>
-        <View style={styles.JoinImgContainer}>
-          <View //Image로 바꿔줘야함!!
-            style={styles.JoinBrandImg}
-            // source={require('../assets/Pool.png')}
-          />
-        </View>
-        <View style={styles.JoinInfoContainer}>
-          <View style={styles.JoinTitleContainer}>
-            <Text style={styles.JoinBrandTitle}>브랜드 등록 신청하기</Text>
-          </View>
-          <View style={styles.JoinIntroContainer}>
-            <Text style={styles.JoinBrandIntro}>
-              누구나 브랜드로 등록하면 메시지를 보낼 수 있어요!
-            </Text>
-          </View>
+      <View style={styles.SettingsContainer}>
+        <View style={styles.SetContainer}>
+          <MainContainer>
+            <View style={styles.SetNotification}>
+              <View>
+                <Text style={styles.NotiText}>알림 수신</Text>
+              </View>
+              <View>
+                <Switch
+                  trackColor={{false: '#767577', true: '#81b0ff'}}
+                  thumbColor={isEnabled ? '#f4f3f4' : '#f4f3f4'}
+                  ios_backgroundColor="#3e3e3e"
+                  onValueChange={toggleSwitch}
+                  value={isEnabled}
+                />
+              </View>
+            </View>
+            <View></View>
+          </MainContainer>
         </View>
       </View>
-      <View style={styles.Settings}></View>
-    </MainContainer>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  UserInfo: {
+  UpperContainer: {
+    flex: 3,
+    marginHorizontal: 24,
+  },
+  SettingsContainer: {
+    flex: 5,
+  },
+  UserInfoContainer: {
     flexDirection: 'row',
     marginTop: 24,
+  },
+  JoinBrandContainer: {
+    flexDirection: 'row',
+    marginTop: 44,
   },
   ProfileImgContainer: {
     flex: 1,
@@ -106,10 +142,6 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '700',
   },
-  JoinBrandContainer: {
-    flexDirection: 'row',
-    marginTop: 44,
-  },
   JoinImgContainer: {
     flex: 1,
   },
@@ -140,7 +172,17 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '400',
   },
-  Settings: {},
+  SetContainer: {
+    backgroundColor: 'tomato',
+  },
+  NotiText: {
+    fontSize: 14,
+    fontWeight: '700',
+  },
+  SetNotification: {
+    flexDirection: 'row',
+    backgroundColor: 'tomato',
+  },
 });
 
 export default SettingStack;
