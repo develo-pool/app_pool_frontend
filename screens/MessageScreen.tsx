85a84ff9c2fcce095d033f201db8ee38e502fa6d
sunbeom <wsbs9805@naver.com>
2022-07-18 17:25:28 +0900

    :art: ScreenBottomButton 사용
    ScreenBottomButton 컴포넌트 재사용 및 유저 종류에 따른 컴포넌트 출력 및 댓글 작성 여부에 따른 컴포넌트 출력
    #3

--- screens/MessageScreen.tsx
+++ screens/MessageScreen.tsx
@@ -1,31 +1,59 @@
 import React from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  GestureResponderEvent,
+} from 'react-native';
 import BorderLine from '../components/message/BorderLine';
 import Comment from '../components/message/Comment';
 import CommentBar from '../components/message/CommentBar';
 import DetailMessageContainer from '../components/message/DetailMessageContainer';
 import InputCommentContainer from '../components/message/InputCommentContainer';
+import ScreenBottomButton from '../components/ScreenBottomButton';
+
+interface User {
+  isBrand: boolean;
+  isWriteComment: boolean;
+}
+const tester: User = {
+  isBrand: false,
+  isWriteComment: true,
+};
 
 function MessageScreen() {
   // const [msgImgHeight, setmsgImgHeight] = useState(0);
   // const { width } = Dimensions.get('window');
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scrollview}>
-        <DetailMessageContainer user={undefined} message={undefined} />
+    <>
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollview}>
+          <DetailMessageContainer user={undefined} message={undefined} />
 
-        <BorderLine />
+          <BorderLine />
 
-        <CommentBar commentCount={927} />
+          <CommentBar commentCount={927} />
 
-        <Comment
-          user={undefined}
-          commentText="프론트짱 다인님의 기운을 받아 햅삐ㅣㅣㅣㅣㅣㅣ"
-          commentDate={Date.now()}
-        />
-      </ScrollView>
-      <InputCommentContainer />
-    </View>
+          <Comment
+            user={undefined}
+            commentText="프론트짱 다인님의 기운을 받아 햅삐ㅣㅣㅣㅣㅣㅣ"
+            commentDate={Date.now()}
+          />
+
+          {tester.isBrand ? (
+            ''
+          ) : tester.isWriteComment ? (
+            <InputCommentContainer />
+          ) : (
+            <ScreenBottomButton
+              name="답장하기"
+              enabled={true}
+              onPress={function (event: GestureResponderEvent): void {}}
+            />
+          )}
+        </ScrollView>
+      </View>
+    </>
   );
 }
 
