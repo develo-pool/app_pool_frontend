171402dc0f7159ed2b82d8d12f46bf4c1304314c
da-in <talown@naver.com>
2022-07-19 11:54:38 +0900

    :art: Exchange BrandProfile & SettingStack Position
    Exchange BrandProfile & SettingStack Position #28

--- screens/MainTab.tsx
+++ screens/MainTab.tsx
@@ -14,10 +14,10 @@ function MainTab() {
     <Tab.Navigator>
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Feed" component={FeedScreen} />
-      <Tab.Screen name="SettingStack" component={SettingStack} />
       {isBrandUser && (
         <Tab.Screen name="BrandProfile" component={BrandProfileScreen} />
       )}
+      <Tab.Screen name="SettingStack" component={SettingStack} />
     </Tab.Navigator>
   );
 }
