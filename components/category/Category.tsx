9182f29361d410db1a522c96f361155bc2c51311
da-in <talown@naver.com>
2022-07-20 16:29:19 +0900

    :sparkles: Category Layout & Function
    Category Layout & Function #39

--- components/category/Category.tsx
+++ components/category/Category.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FlatList, StyleSheet} from 'react-native';
 import Title from '../Title';
 import CategoryItem, {CategoryItemProps} from './CategoryItem';
 import categoryList from './categoryList';
 
 function Category() {
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
+
+  const checkedItemHandler = (name: string, isChecked: boolean) => {
+    if (isChecked) {
+      setCheckedItems([...checkedItems, name]);
+    } else if (!isChecked && checkedItems.find(i => i === name)) {
+      const nextCheckedItems = checkedItems.filter(i => i !== name);
+      setCheckedItems(nextCheckedItems);
+    }
+  };
+
+  const renderItem = ({item}: {item: CategoryItemProps}) => (
+    <CategoryItem
+      item={item}
+      checkedItemHandler={checkedItemHandler}
+      checkedItems={checkedItems}
+    />
+  );
+
   return (
     <>
       <Title title="콘텐츠 카테고리" alignCenter={true} />
@@ -19,10 +38,6 @@ function Category() {
     </>
   );
 }
-
-const renderItem = ({item}: {item: CategoryItemProps}) => (
-  <CategoryItem item={item} />
-);
 
 const styles = StyleSheet.create({
   block: {flex: 1},
