20bc9e4ec657110bb071824713299acea8b5052b
da-in <talown@naver.com>
2022-07-21 15:36:33 +0900

    :ambulance: Setting react-query
    Setting react-query #41

--- App.tsx
+++ App.tsx
@@ -2,11 +2,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import RootStack from './screens/RootStack';
 
+const reactQuery = require('react-query');
+const {QueryClient, QueryClientProvider} = reactQuery;
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
