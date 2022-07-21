0cf4fa78ecb4f482249080d498f8bcfa99fa1239
Orca1110 <hoony4509@naver.com>
2022-07-21 12:27:06 +0900

    :zap: Add function that make clear button appear/disappear
    Yeap #31

--- components/TextInputs.tsx
+++ components/TextInputs.tsx
@@ -1,21 +1,43 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
 import theme from '../theme';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function TextInputs() {
   const [text, setText] = useState('');
+  const [empty, setEmpty] = useState(true);
 
+  const onPress = () => {
+    setText('');
+  };
+
+  const emptyCheck = () => {
+    {
+      console.log(text);
+      text ? setEmpty(false) : setEmpty(true);
+    }
+  };
   return (
     <View style={[styles.Textinput, styles.Default]}>
       <TextInput
         style={styles.InputStyle}
         placeholder="Text"
         value={text}
-        onChangeText={value => setText(value)}
+        onChange={emptyCheck}
+        onChangeText={setText}
       />
-      <TouchableOpacity onPress={() => setText('')}>
-        <Icon name="clear" size={20} />
+      <TouchableOpacity onPress={onPress}>
+        {empty ? (
+          <Icon name="clear" size={20} style={styles.isEmpty} />
+        ) : (
+          <Icon name="clear" size={20} />
+        )}
       </TouchableOpacity>
     </View>
   );
@@ -53,6 +75,9 @@ const styles = StyleSheet.create({
     fontSize: theme.fontSize.P1,
     marginLeft: 4,
     width: 292,
+  },
+  isEmpty: {
+    display: 'none',
   },
 });
 
