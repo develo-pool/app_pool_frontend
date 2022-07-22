e70185b50d1af25693f4859fdb47afdb1e1a8618
da-in <talown@naver.com>
2022-07-22 21:44:03 +0900

    :lipstick: Update TextInputs UI
    Change Width Flexable & Simple, Add Correct, Error Text Props #47

--- components/TextInputs.tsx
+++ components/TextInputs.tsx
@@ -1,63 +1,94 @@
 import React from 'react';
-import {View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import theme from '../theme';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 interface Props {
-  type: 'default' | 'disable' | 'focus' | 'correct' | 'error';
+  type: 'default' | 'disable' | 'focus' | 'error';
   placeholder: string;
   value?: string;
   onChangeText?: any;
+  alert?: {
+    type: 'Correct' | 'Error';
+    text: string;
+  };
 }
 
-function TextInputs({type, placeholder, onChangeText, value}: Props) {
+function TextInputs({type, placeholder, onChangeText, value, alert}: Props) {
   return (
-    <View style={[styles.Textinput, styles[type]]}>
-      <TextInput
-        style={styles.inputStyle}
-        placeholder={placeholder}
-        value={value}
-        onChangeText={onChangeText}
-      />
-      <TouchableOpacity onPress={() => onChangeText('')}>
-        {value && <Icon name="clear" size={20} />}
-      </TouchableOpacity>
+    <View style={[styles.block]}>
+      <View style={[styles.Textinput, styles[type]]}>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder={placeholder}
+          value={value}
+          onChangeText={onChangeText}
+        />
+        <TouchableOpacity onPress={() => onChangeText('')}>
+          {value && (
+            <Icon name="clear" style={styles.clear} size={12} color="white" />
+          )}
+        </TouchableOpacity>
+      </View>
+      {alert && (
+        <View style={styles.textContainer}>
+          <Text style={{color: theme.colors[alert.type]}}>{alert.text}</Text>
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  Textinput: {
+  block: {
     flex: 1,
+    flexDirection: 'column',
+  },
+  Textinput: {
     height: 46,
     borderWidth: 1,
     borderRadius: 6,
+    paddingHorizontal: 12,
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-  },
-  default: {
     borderColor: theme.colors.Grey30,
-    backgroundColor: theme.colors.White,
-  },
-  disable: {
-    borderColor: theme.colors.Grey30,
-    backgroundColor: theme.colors.Grey10,
-  },
-  focus: {
-    borderColor: theme.colors.Black,
-    backgroundColor: theme.colors.White,
-  },
-  correct: {
-    borderColor: theme.colors.Grey30,
-  },
-  error: {
-    borderColor: theme.colors.Error,
   },
   inputStyle: {
     fontFamily: theme.fontFamily.Pretendard,
     fontSize: theme.fontSize.P1,
-    marginLeft: 4,
-    width: 292,
+  },
+  default: {
+    backgroundColor: theme.colors.White,
+  },
+  disable: {
+    backgroundColor: theme.colors.Grey10,
+  },
+  focus: {
+    borderColor: theme.colors.Black,
+  },
+  error: {
+    borderColor: theme.colors.Error,
+  },
+  clear: {
+    backgroundColor: theme.colors.Grey30,
+    borderRadius: 10,
+    padding: 2,
+  },
+  textContainer: {
+    paddingLeft: 6,
+    marginTop: 4,
+    backgroundColor: 'pink',
+  },
+  text: {
+    fontSize: 12,
+    paddingLeft: 6,
   },
 });
 
