15c923e82fd5a1d6ddbe8ca973dc880c2c04f58c
da-in <talown@naver.com>
2022-07-27 15:44:24 +0900

    :bug: Fix Theme Path
    Fix Theme Path #70

--- components/create/SendButton.tsx
+++ components/create/SendButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text, Pressable, StyleSheet} from 'react-native';
-import theme from '../../theme';
+import theme from '../../assets/theme';
 
 export function SendButton({
   text,
