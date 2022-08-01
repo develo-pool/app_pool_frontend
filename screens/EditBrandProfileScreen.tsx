d5c467f09400b52197f1e734b65adbe15992b085
Orca1110 <hoony4509@naver.com>
2022-08-01 15:20:57 +0900

    :seedling: EditScreen and WelcomeMasgScreen Route Added
    #89

---
+++ screens/EditBrandProfileScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, StyleSheet, TextInput, Pressable} from 'react-native';
+import theme from '../assets/theme';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+function EditBrandProfile() {
+  return <></>;
+}
+
+export default EditBrandProfile;
