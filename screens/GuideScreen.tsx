602199d6f53992f856a7cc8c8e7fd1a49193ca98
da-in <talown@naver.com>
2022-08-08 21:15:04 +0900

    :lipstick: Guide Screen Publishing
    Guide Screen Publishing #119

--- screens/GuideScreen.tsx
+++ screens/GuideScreen.tsx
@@ -1,32 +1,48 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import theme from '../assets/theme';
 import Carousel from '../components/guide/Carousel';
-import MainContainer from '../components/MainContainer';
 import ScreenBottomButton from '../components/ScreenBottomButton';
 import {RootStackNavigationProp} from './types';
 
 function GuideScreen() {
   const navigation = useNavigation<RootStackNavigationProp>();
+  const [index, setIndex] = useState<number>(0);
   return (
-    <View style={styles.background}>
-      <Carousel />
-      <MainContainer>{}</MainContainer>
-      <ScreenBottomButton
-        name="건너뛰기"
-        onPress={() => navigation.navigate('MainTab')}
-      />
-    </View>
+    <>
+      <Carousel index={index} setIndex={setIndex} />
+      {index === 3 ? (
+        <ScreenBottomButton
+          name="시작하기"
+          onPress={() => navigation.navigate('MainTab')}
+        />
+      ) : (
+        <View style={styles.skipContainer}>
+          <Pressable onPress={() => navigation.navigate('MainTab')}>
+            <Text style={styles.skip}>사용법 건너뛰기</Text>
+          </Pressable>
+        </View>
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: '#D3D3D3',
-  },
   center: {
     textAlign: 'center',
+  },
+  skipContainer: {
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: theme.colors.White,
+  },
+  skip: {
+    fontFamily: theme.fontFamily.Pretendard,
+    fontSize: theme.fontSize.P2,
+    fontWeight: theme.fontWeight.Bold,
+    color: theme.colors.Poolgreen,
   },
 });
 
