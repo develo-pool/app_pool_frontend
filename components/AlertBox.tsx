60e754b503e92c6ea999e25ad550094b1a916c33
da-in <talown@naver.com>
2022-08-10 20:16:07 +0900

    :sparkles: Create Alert Component & Hooks
    Create Alert Component & Hooks #140

---
+++ components/AlertBox.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import theme from '../assets/theme';
+import {useSelector} from 'react-redux';
+import {PADDING} from './MainContainer';
+import {RootState} from '../slices';
+
+function AlertBox() {
+  const alertState = useSelector((state: RootState) => state.alert);
+  return (
+    <>
+      {alertState.isVisible && (
+        <View
+          style={[
+            styles.box,
+            alertState.alert.type === 'Error' ? styles.red : styles.green,
+          ]}>
+          <Text
+            style={[
+              styles.text,
+              alertState.alert.type === 'Error' ? styles.red : styles.green,
+            ]}>
+            {alertState.alert.text}
+          </Text>
+        </View>
+      )}
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  box: {
+    position: 'absolute',
+    top: 16,
+    width: '100%',
+    marginHorizontal: PADDING,
+    paddingHorizontal: 16,
+    paddingVertical: 19,
+    borderRadius: 8,
+    zIndex: 10,
+  },
+  text: {
+    fontFamily: theme.fontFamily.Pretendard,
+    fontSize: theme.fontSize.P2,
+    lineHeight: 14,
+  },
+  red: {
+    backgroundColor: '#FFE4E6',
+    color: theme.colors.Error,
+  },
+  green: {
+    backgroundColor: '#D7FFF3',
+    color: theme.colors.Correct,
+  },
+});
+
+export default AlertBox;
