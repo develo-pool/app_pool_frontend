846e4b4cbc4a4605e8a855bed5d33c103226030f
da-in <talown@naver.com>
2022-08-10 20:51:34 +0900

    :sparkles: Alert After Fail to Check User(Password)
    Alert After Fail to Check User(Password) #140

--- screens/PasswordScreen.tsx
+++ screens/PasswordScreen.tsx
@@ -1,10 +1,17 @@
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useMutation, useQuery} from 'react-query';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {checkMember, updatePassword} from '../api/auth';
+import AlertBox from '../components/AlertBox';
 import {InputTitle} from '../components/auth/AuthComponents';
 import PasswordForm from '../components/auth/PasswordForm';
 import PhoneAuthForm from '../components/auth/PhoneAuthForm';
@@ -14,10 +21,14 @@ import ProcessBar from '../components/ProcessBar';
 import ScreenBottomButton from '../components/ScreenBottomButton';
 import TextInputs from '../components/TextInputs';
 import Title from '../components/Title';
+import {RootState} from '../slices';
 import {createAlert, deleteAlert} from '../slices/alert';
 import {RootStackNavigationProp, RootStackParamList} from './types';
 
 const TOTAL = 2;
+
+const headerHeight =
+  Dimensions.get('screen').height - Dimensions.get('window').height;
 
 interface PasswordScreenProps {
   username: string;
@@ -35,6 +46,7 @@ type PasswordScreenRouteProp = RouteProp<RootStackParamList, 'Password'>;
 
 function PasswordScreen() {
   const dispatch = useDispatch();
+  const isVisible = useSelector((state: RootState) => state.alert.isVisible);
   const route = useRoute<PasswordScreenRouteProp>();
   const current = route.params.current;
   const navigation = useNavigation<RootStackNavigationProp>();
@@ -63,7 +75,17 @@ function PasswordScreen() {
         if (value.data) {
           navigation.navigate('Password', {current: 1});
         } else {
-          console.log('다시 시도해주세요.');
+          navigation.setOptions({headerShown: false});
+          dispatch(
+            createAlert({
+              type: 'Error',
+              text: '일치하는 회원정보가 없습니다. 다시 입력해 주세요.',
+            }),
+          );
+          setTimeout(() => {
+            dispatch(deleteAlert());
+            navigation.setOptions({headerShown: true});
+          }, 3500);
         }
       });
     },
@@ -76,7 +98,7 @@ function PasswordScreen() {
       dispatch(
         createAlert({type: 'Complete', text: '비밀번호가 재설정되었습니다.'}),
       );
-      setTimeout(() => dispatch(deleteAlert()), 5000);
+      setTimeout(() => dispatch(deleteAlert()), 3500);
       navigation.reset({routes: [{name: 'Login'}]});
     },
   });
@@ -108,6 +130,7 @@ function PasswordScreen() {
   return (
     <>
       <MainContainer>
+        <AlertBox />
         {current ? (
           <ScrollView
             showsVerticalScrollIndicator={false}
@@ -126,7 +149,7 @@ function PasswordScreen() {
           <ScrollView
             showsVerticalScrollIndicator={false}
             keyboardShouldPersistTaps="handled">
-            <View style={styles.block}>
+            <View style={[styles.block, isVisible && styles.alert]}>
               <Title title="본인인증을" />
               <Title title="진행해주세요." hasMargin={true} />
               <InputTitle title="아이디" />
@@ -200,6 +223,9 @@ const styles = StyleSheet.create({
   },
   noMargin: {
     marginBottom: 2,
+  },
+  alert: {
+    marginTop: 92 + headerHeight,
   },
 });
 
