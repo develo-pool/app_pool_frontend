7aebac28a7fef481558301da928212a225dac122
Sunbeom <wsbs9805@naver.com>
2022-08-19 13:58:04 +0900

    contexts는 사용하지 않습니다.

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import RootStack from './screens/RootStack';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
-// import {LogContextProVier} from './contexts/LogContext'
 import store from './slices';
 
 const queryClient = new QueryClient();
