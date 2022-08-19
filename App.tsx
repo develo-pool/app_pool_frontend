b883d98b95e24f5dfa5228e4df03cd9ce3e335d2
Sunbeom <wsbs9805@naver.com>
2022-08-19 13:45:22 +0900

    :art: Context작업중

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import RootStack from './screens/RootStack';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
+// import {LogContextProVier} from './contexts/LogContext'
 import store from './slices';
 
 const queryClient = new QueryClient();
