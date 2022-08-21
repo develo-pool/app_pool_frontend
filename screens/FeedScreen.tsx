529ca645859383ebaa952d45640a04fa5b56ddcb
Sunbeom <wsbs9805@naver.com>
2022-08-22 05:31:18 +0900

    :bug: 린선생님 잡기
    #155

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -5,7 +5,6 @@ import theme from '../assets/theme';
 import NowDate from '../components/feed/NowDate';
 import Hello from '../components/feed/Hello';
 import {getUser} from '../api/auth';
-// get방식 -> useQuery
 import {useQuery} from 'react-query';
 import {getAllMessage} from '../api/message/index';
 
@@ -13,17 +12,17 @@ function FeedScreen() {
   const {data: userData} = useQuery('getUserResult', () => getUser(), {
     refetchOnMount: 'always',
   });
-  console.log(userData)
+  console.log(userData);
   const {data: allMessageData, refetch} = useQuery(
     'getAllMessage',
     () => getAllMessage(),
-    {enabled: false,}
+    {enabled: false},
   );
   useEffect(() => {
-    if (userData?.userFollowingCount !== 0){
-      refetch()
+    if (userData?.userFollowingCount !== 0) {
+      refetch();
     }
-  }, []);
+  }, [userData, refetch]);
   const today = new Date().toLocaleDateString().replace(/\./g, '');
   const yy = today.substring(6, 8);
   const dd = today.substring(3, 5);
@@ -34,10 +33,6 @@ function FeedScreen() {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        {/* <Pressable onPress={() => refetchGetUser()}>
-          <Text>요청</Text>
-        </Pressable> */}
-
         <ScrollView showsVerticalScrollIndicator={false}>
           <Hello name={userData?.nickName} />
           <NowDate msgDate={yymmdd} />
@@ -88,7 +83,6 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    // API 붙이고 화면 너비 높이 계산해서 중앙으로...!
     marginTop: 200,
   },
   isMessageContainer: {
