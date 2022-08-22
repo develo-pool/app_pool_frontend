cfe53fdb415ac837b5f115f2b947f32882250a08
Sunbeom <wsbs9805@naver.com>
2022-08-22 19:00:35 +0900

    불확실한 해결방법...
    내일 좀 더 해보자 ㅠ

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import RootStack from './screens/RootStack';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
 import store from './slices';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import {AppState, Platform} from 'react-native';
 
 const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    const listener = AppState.addEventListener('change', status => {
+      if (Platform.OS === 'ios' && status === 'active') {
+        request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY)
+          .then(result => console.log(result))
+          .catch(error => console.log(error));
+      }
+    });
+
+    return listener.remove;
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
