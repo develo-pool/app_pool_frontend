b3fff471faf0b5e4d5484d978258c8c19dc753c5
Orca1110 <hoony4509@naver.com>
2022-08-22 19:45:12 +0900

    :art: Starting FCM
    #201

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Alert} from 'react-native';
 import RootStack from './screens/RootStack';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
 import store from './slices';
+import messaging from '@react-native-firebase/messaging';
 
 const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
