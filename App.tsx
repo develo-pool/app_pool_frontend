375a7dd6143b83bc87830dc81c4a15060f248a73
Sunbeom <wsbs9805@naver.com>
2022-08-22 19:00:35 +0900

    불확실한 해결방법...
    내일 좀 더 해보자 ㅠ

--- App.tsx
+++ App.tsx
@@ -1,16 +1,24 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React, {useEffect} from 'react';
+
 import {Alert} from 'react-native';
+
 import RootStack from './screens/RootStack';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
 import store from './slices';
+
 import messaging from '@react-native-firebase/messaging';
+
+import {request, PERMISSIONS} from 'react-native-permissions';
+import {AppState, Platform} from 'react-native';
+
 
 const queryClient = new QueryClient();
 
 function App() {
   useEffect(() => {
+
     messaging().onNotificationOpenedApp(remoteMessage => {
       console.log(
         'Notification caused app to open from background state:',
@@ -38,6 +46,18 @@ function App() {
       console.log(remoteMessage);
     });
     return unsubscribe;
+  }, []);
+
+
+    const listener = AppState.addEventListener('change', status => {
+      if (Platform.OS === 'ios' && status === 'active') {
+        request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY)
+          .then(result => console.log(result))
+          .catch(error => console.log(error));
+      }
+    });
+
+    return listener.remove;
   }, []);
 
   return (
