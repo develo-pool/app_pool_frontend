1c9a33f0af0f8689c7bb316e7cff47b2696dfe3f
Sunbeom <wsbs9805@naver.com>
2022-08-22 15:55:30 +0900

    개인정보추적요청세팅시작
    #197

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -31,6 +31,7 @@ import {useIsFocused} from '@react-navigation/native';
 
 const LENGTH = 10;
 
+
 function FeedScreen() {
   const [cursor, setCursor] = useState<number>(0);
   const [Messages, setMessages] = useState<Message[]>([]);
