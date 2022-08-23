ddcb942f24bf80014bb8197f93c6a5847dd085c8
Sunbeom <wsbs9805@naver.com>
2022-08-23 14:39:28 +0900

    일단 알림, 추적요청 가능
    가-능

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import RootStack from './screens/RootStack';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
 import store from './slices';
-import {request, PERMISSIONS, requestMultiple} from 'react-native-permissions';
+import {
+  request,
+  PERMISSIONS,
+  requestMultiple,
+  requestNotifications,
+} from 'react-native-permissions';
 import {AppState, Platform} from 'react-native';
 
 import messaging from '@react-native-firebase/messaging';
@@ -29,6 +34,21 @@ requestUserPermission();
 
 function App() {
   useEffect(() => {
+    // requestMultiple([
+    //   PERMISSIONS.IOS.CAMERA,
+    //   PERMISSIONS.IOS.PHOTO_LIBRARY,
+    //   PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY,
+    // ]).then(statuses => {
+    //   console.log('Camera', statuses[PERMISSIONS.IOS.CAMERA]);
+    //   console.log('Photo_Library)', statuses[PERMISSIONS.IOS.PHOTO_LIBRARY]);
+    //   console.log(
+    //     'App_tracking_Transparency',
+    //     statuses[PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY],
+    //   );
+    // });
+    // requestNotifications(['alert', 'sound', 'badge']).then(
+    //   ({status, settings}) => {},
+    // );
     const listener = AppState.addEventListener('change', status => {
       if (Platform.OS === 'ios' && status === 'active') {
         request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY)
@@ -36,14 +56,6 @@ function App() {
           .catch(error => console.log(error));
       }
     });
-    requestMultiple([PERMISSIONS.IOS.CAMERA, PERMISSIONS.IOS.FACE_ID, PERMISSIONS.IOS.PHOTO_LIBRARY, PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY]).then(
-      statuses => {
-        console.log('Camera', statuses[PERMISSIONS.IOS.CAMERA]);
-        console.log('FaceID', statuses[PERMISSIONS.IOS.FACE_ID]);
-        console.log('Photo_Library)', statuses[PERMISSIONS.IOS.PHOTO_LIBRARY]);
-        console.log('App_tracking_Transparency', statuses[PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY]);
-      },
-    );
     return listener.remove;
   }, []);
 
