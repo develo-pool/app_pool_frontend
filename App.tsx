7d8a3fbe6ee380546305c9bd9c3b65d991b29189
Orca1110 <hoony4509@naver.com>
2022-08-23 17:03:20 +0900

    :art: Receiving Codes Added
    #201

--- App.tsx
+++ App.tsx
@@ -17,6 +17,29 @@ function App() {
     return unsubscribe;
   }, []);
 
+  useEffect(() => {
+    // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+        }
+      });
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
