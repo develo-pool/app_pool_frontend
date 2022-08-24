d01b6935256838520a4ff39bdf8fb74c353e8288
da-in <talown@naver.com>
2022-08-24 14:02:31 +0900

    :children_crossing: Modify Loding Logic & NotFound UI
    Modify Loding Logic & NotFound UI #216

--- web/NotFound.web.tsx
+++ web/NotFound.web.tsx
@@ -7,10 +7,12 @@ function NotFound() {
   return (
     <>
       <View style={styles.block}>
-        <Text>프로필 URL이 올바르지 않습니다.</Text>
-        <Text>앱으로 접속하시거나, 브랜드에 문의해 주세요.</Text>
+        <Text
+          style={
+            styles.text
+          }>{`프로필 URL이 올바르지 않습니다.${'\n'}앱으로 접속하시거나, 브랜드에 문의해 주세요.`}</Text>
       </View>
-      <View style={styles.Ivory}>
+      <View style={styles.ivory}>
         <Footer />
       </View>
     </>
@@ -23,8 +25,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  Ivory: {
+  ivory: {
     backgroundColor: theme.colors.Ivory,
+  },
+  text: {
+    color: theme.colors.Grey50,
+    textAlign: 'center',
   },
 });
 
