91378ff08cbc864a803fe91ce9832618ccb7239d
Sunbeom <wsbs9805@naver.com>
2022-08-29 14:38:44 +0900

    FirebaseAuth 활성

--- components/auth/PhoneAuthForm.tsx
+++ components/auth/PhoneAuthForm.tsx
@@ -3,7 +3,7 @@ import {StyleSheet, View} from 'react-native';
 import TextInputs from '../TextInputs';
 import {AuthButton, InputTitle} from './AuthComponents';
 import {CheckPhoneNumber} from './Validation';
-// import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import {phoneNumberExist} from '../../api/auth';
 import {useQuery} from 'react-query';
 
@@ -30,8 +30,8 @@ function PhoneAuthForm({
     CHANGE_PASSWORD: true,
   };
   const [phoneNumberValid, setPhoneNumberValid] = useState<boolean>(true);
-  // const [confirmation, setConfirmation] =
-  //   useState<FirebaseAuthTypes.ConfirmationResult>();
+  const [confirmation, setConfirmation] =
+    useState<FirebaseAuthTypes.ConfirmationResult>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isExist, setIsExist] = useState<boolean>(false);
   const changePhoneNumberHandler = (value: string) => {
@@ -41,27 +41,27 @@ function PhoneAuthForm({
   };
   const authLen = form.authNumber.length;
   const requestAuthNumber = async () => {
-    // setIsLoading(true);
-    // const response = await auth().signInWithPhoneNumber(
-    //   `+82${form.phoneNumber.substring(1)}`,
-    // );
-    // setIsLoading(false);
-    // setConfirmation(response);
+    setIsLoading(true);
+    const response = await auth().signInWithPhoneNumber(
+      `+82${form.phoneNumber.substring(1)}`,
+    );
+    setIsLoading(false);
+    setConfirmation(response);
     onChangeForm('state')('request');
   };
   const verifyAuthNumber = async () => {
-    // try {
-    // setIsLoading(true);
-    //   const data = await confirmation?.confirm(form.authNumber);
+    try {
+    setIsLoading(true);
+      const data = await confirmation?.confirm(form.authNumber);
     setIsLoading(false);
-    //   if (data !== undefined) {
+      if (data !== undefined) {
     onChangeForm('state')('confirm');
-    //   }
-    // } catch (error) {
-    //   onChangeForm('authNumberError')(true);
-    // } finally {
-    //   await auth().currentUser?.delete();
-    // }
+      }
+    } catch (error) {
+      onChangeForm('authNumberError')(true);
+    } finally {
+      await auth().currentUser?.delete();
+    }
   };
   const {refetch} = useQuery(
     ['phoneNumberExist', form.phoneNumber],
