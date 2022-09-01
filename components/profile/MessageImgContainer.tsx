163ac6070054cc5f0457047def3cc458c142a4ec
Orca1110 <hoony4509@naver.com>
2022-09-01 15:02:35 +0900

    :art: Message Contianers Resize Images

--- components/profile/MessageImgContainer.tsx
+++ components/profile/MessageImgContainer.tsx
@@ -4,15 +4,14 @@ import {Dimensions, Image, StyleSheet, View} from 'react-native';
 interface Props {
   messageImg: string;
 }
+const {width} = Dimensions.get('window');
 
 function MessageImgContainer({messageImg}: Props) {
   // 높이를 정해주기 위한 height useState와 Dimensions 사용
   const [height, setHeight] = useState(0);
   if (messageImg !== '') {
-    const {width} = Dimensions.get('window');
-    // 아래는 이미지의 가로 세로를 가져와 setHeight로 높이 설정해주는..!
     Image.getSize(messageImg, (w, h) => {
-      setHeight(h * ((width - 32) / w));
+      h >= w ? setHeight(width - 74) : setHeight(h * ((width - 74) / w));
     });
   }
 
