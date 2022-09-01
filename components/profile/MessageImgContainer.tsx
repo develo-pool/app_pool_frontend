60e558bea8d4aa7cd05aac7d9173bc365b1e7623
Orca1110 <hoony4509@naver.com>
2022-09-01 17:24:24 +0900

    :art: Resizing Image

--- components/profile/MessageImgContainer.tsx
+++ components/profile/MessageImgContainer.tsx
@@ -4,14 +4,18 @@ import {Dimensions, Image, StyleSheet, View} from 'react-native';
 interface Props {
   messageImg: string;
 }
-const {width} = Dimensions.get('window');
+const windowWidth = Dimensions.get('window').width;
 
 function MessageImgContainer({messageImg}: Props) {
   // 높이를 정해주기 위한 height useState와 Dimensions 사용
   const [height, setHeight] = useState(0);
+  console.log(windowWidth - 56);
+
   if (messageImg !== '') {
     Image.getSize(messageImg, (w, h) => {
-      h >= w ? setHeight(width - 74) : setHeight(h * ((width - 74) / w));
+      h >= w
+        ? (setHeight(windowWidth / 2), console.log(height))
+        : setHeight(h * (windowWidth / 2 / w));
     });
   }
 
@@ -20,7 +24,7 @@ function MessageImgContainer({messageImg}: Props) {
       <Image
         style={styles.messageImg}
         source={{uri: messageImg !== '' ? messageImg : undefined, height}}
-        resizeMode="contain"
+        resizeMode="cover"
       />
     </View>
   );
@@ -28,13 +32,14 @@ function MessageImgContainer({messageImg}: Props) {
 
 const styles = StyleSheet.create({
   imageContainer: {
-    maxHeight: 220,
+    maxHeight: windowWidth + 56,
     marginBottom: 8,
+    overflow: 'hidden',
+    justifyContent: 'center',
   },
   messageImg: {
     borderRadius: 6,
-    maxHeight: 220,
-    maxWidth: 400,
+    maxWidth: windowWidth / 2,
   },
 });
 
