2b5ee25495eb74a01c1636751354c20e921719c4
Orca1110 <hoony4509@naver.com>
2022-09-01 18:27:32 +0900

    :art: Image Sizing Done

--- components/profile/MessageImgContainer.tsx
+++ components/profile/MessageImgContainer.tsx
@@ -9,13 +9,12 @@ const windowWidth = Dimensions.get('window').width;
 function MessageImgContainer({messageImg}: Props) {
   // 높이를 정해주기 위한 height useState와 Dimensions 사용
   const [height, setHeight] = useState(0);
-  console.log(windowWidth - 56);
 
   if (messageImg !== '') {
     Image.getSize(messageImg, (w, h) => {
       h >= w
-        ? (setHeight(windowWidth / 2), console.log(height))
-        : setHeight(h * (windowWidth / 2 / w));
+        ? setHeight(windowWidth - 56)
+        : setHeight(h * ((windowWidth - 56) / w));
     });
   }
 
@@ -32,14 +31,14 @@ function MessageImgContainer({messageImg}: Props) {
 
 const styles = StyleSheet.create({
   imageContainer: {
-    maxHeight: windowWidth + 56,
+    maxHeight: windowWidth,
     marginBottom: 8,
     overflow: 'hidden',
     justifyContent: 'center',
   },
   messageImg: {
     borderRadius: 6,
-    maxWidth: windowWidth / 2,
+    maxWidth: windowWidth,
   },
 });
 
