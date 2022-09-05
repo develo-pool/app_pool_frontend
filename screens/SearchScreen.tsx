b8debb4dc4eb3b897bba0d98ecf20d904739ea53
Sunbeom <wsbs9805@naver.com>
2022-09-05 14:53:10 +0900

    키보드 높이 잡아내는중

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -5,6 +5,7 @@ import {
   SafeAreaView,
   FlatList,
   ActivityIndicator,
+  Keyboard,
 } from 'react-native';
 import SearchBar from '../components/search/SearchBar';
 import RecommandBrandUserContainer from '../components/search/RecommandBrandUserContainer';
@@ -20,6 +21,8 @@ import {useIsFocused} from '@react-navigation/native';
 const LENGTH = 10;
 
 function SearchScreen() {
+  const [keyboardHeight, setKeyboardHeight] = useState<number>(0);
+
   const [cursor, setCursor] = useState<number>(0);
   const [Brands, setBrands] = useState<AllBrandResult[]>([]);
   const [noMoreBrand, setNoMoreBrand] = useState<boolean>(false);
@@ -50,6 +53,7 @@ function SearchScreen() {
       },
     },
   );
+
   const onEndReached = () => {
     if (!noMoreBrand) {
       refetch();
@@ -63,7 +67,7 @@ function SearchScreen() {
   }, [refetch]);
   useEffect(() => {
     refetch();
-  }, [refetch, isFocused]);
+  }, [refetch, isFocused, searchText]);
   const RenderRecommandItem = ({item}) => {
     return (
       <RecommandBrandUserContainer
