5f1e54544cafed2b58382c3919368a38c64b6c67
da-in <talown@naver.com>
2022-09-06 14:59:04 +0900

    :bug: Password Screen Keyboard Avoiding View
    Password Screen Keyboard Avoiding View #292

--- screens/PasswordScreen.tsx
+++ screens/PasswordScreen.tsx
@@ -144,72 +144,72 @@ function PasswordScreen() {
 
   return (
     <>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        keyboardShouldPersistTaps="handled"
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <MainContainer>
-          <SafeAreaView style={styles.front}>
-            <View>
-              <AlertBox />
-            </View>
-          </SafeAreaView>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.inner}>
-              {current ? (
-                <View style={styles.block}>
-                  <Title title="비밀번호를" />
-                  <Title title="재설정해 주세요." hasMargin={true} />
-                  <PasswordForm
-                    form={form}
-                    onChangeForm={createChangeFormHandler}
-                    setForm={setForm}
-                  />
-                </View>
-              ) : (
-                <View style={[styles.block, isVisible && styles.alert]}>
-                  <Title title="본인인증을" />
-                  <Title title="진행해주세요." hasMargin={true} />
-                  <InputTitle title="아이디" />
-                  <View style={styles.row}>
-                    <TextInputs
-                      type={
-                        form.username.length > 2 || !form.username
-                          ? 'default'
-                          : 'error'
-                      }
-                      placeholder="아이디를 입력해 주세요"
-                      value={form.username}
-                      onChangeText={(value: string) =>
-                        setForm({
-                          ...form,
-                          username: ReplaceKorean(value),
-                          usernameChecked: undefined,
-                        })
-                      }
-                      maxLength={20}
-                      alert={
-                        form.username.length > 2 || !form.username
-                          ? undefined
-                          : {type: 'Error', text: '3자 이상 입력해주세요.'}
-                      }
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={styles.container}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={styles.contentContainer}>
+          <MainContainer>
+            <SafeAreaView style={styles.front}>
+              <View>
+                <AlertBox />
+              </View>
+            </SafeAreaView>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <View style={styles.inner}>
+                {current ? (
+                  <View style={styles.block}>
+                    <Title title="비밀번호를" />
+                    <Title title="재설정해 주세요." hasMargin={true} />
+                    <PasswordForm
+                      form={form}
+                      onChangeForm={createChangeFormHandler}
+                      setForm={setForm}
                     />
                   </View>
-                  <PhoneAuthForm
-                    form={form}
-                    onChangeForm={createChangeFormHandler}
-                    setForm={setForm}
-                    mode="CHANGE_PASSWORD"
-                  />
-                </View>
-              )}
-            </View>
-          </TouchableWithoutFeedback>
-        </MainContainer>
-      </ScrollView>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+                ) : (
+                  <View style={[styles.block, isVisible && styles.alert]}>
+                    <Title title="본인인증을" />
+                    <Title title="진행해주세요." hasMargin={true} />
+                    <InputTitle title="아이디" />
+                    <View style={styles.row}>
+                      <TextInputs
+                        type={
+                          form.username.length > 2 || !form.username
+                            ? 'default'
+                            : 'error'
+                        }
+                        placeholder="아이디를 입력해 주세요"
+                        value={form.username}
+                        onChangeText={(value: string) =>
+                          setForm({
+                            ...form,
+                            username: ReplaceKorean(value),
+                            usernameChecked: undefined,
+                          })
+                        }
+                        maxLength={20}
+                        alert={
+                          form.username.length > 2 || !form.username
+                            ? undefined
+                            : {type: 'Error', text: '3자 이상 입력해주세요.'}
+                        }
+                      />
+                    </View>
+                    <PhoneAuthForm
+                      form={form}
+                      onChangeForm={createChangeFormHandler}
+                      setForm={setForm}
+                      mode="CHANGE_PASSWORD"
+                    />
+                  </View>
+                )}
+              </View>
+            </TouchableWithoutFeedback>
+          </MainContainer>
+        </ScrollView>
         {current ? (
           <ScreenBottomButton
             name="재설정 완료"
