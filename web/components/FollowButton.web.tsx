886da4a589f8f940d54ca6b0c4186f79130af56b
da-in <talown@naver.com>
2022-10-06 18:40:01 +0900

    :sparkles: openStoreByOS Hook
    openStoreByOS Hook #330

--- web/components/FollowButton.web.tsx
+++ web/components/FollowButton.web.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import theme from '../../assets/theme';
-import openStoreByOS from '../hooks/openStoreByOS';
+import openStoreByOS from '../hooks/openStoreByOS.web';
 // import {MdCheckCircle} from 'react-icons/md';
 
 function FollowButton() {
